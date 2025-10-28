d1152154bc17d11603eb2e20f70cb831828944c5
Devon Seger <devon.seger@gmail.com>
2025-10-28 12:41:34 -0500

    Start work on the login route/logic.

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -14,10 +14,29 @@ route
     let user = await new UserService().getUserById(userId)
     res.json(user)
   })
-  .post("/", async (req, res) => {
+  .post("/register", async (req, res) => {
     const data = req.body
     await new UserService().createUser(data)    
     res.json('User created successfully')
+  })
+  .post("/login", async (req, res) => {
+    const { user, password } = req.body
+    try {
+      const userService = new UserService();
+      const users = await userService.getAllUsers();
+      const foundUser = users.find((u) => u.user === user)
+
+      if (!foundUser) {
+        res.json("Incorrect username or password.")
+      }
+
+      const validPass = await bcrpyt.compare(validPass, foundUser.password )
+      if (!validPass) {
+        res.json("Incorrect username or password.")
+      }
+    } catch (error) {
+      
+    }
   })
   .patch("/", async (req, res) => {
     console.log("User put route");
